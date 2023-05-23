ea80a4c16d91393112aafbf196f2762ff06ac83b
MUYEONKIM <rlaandus2@gmail.com>
2023-05-23 16:49:49 +0900

    mutation에 전부 보내는게 아니라 내가 수정한 것만 보내기 해결방법: 빈객체를 만들어서 하기 (중요)

---
+++ pages/section09/09-04-boards/[number]/index.js
@@ -0,0 +1,39 @@
+import { useQuery, gql } from '@apollo/client'
+import { useRouter } from 'next/router'
+const FETCH_BOARD = gql`
+    query fetchBoard($number: Int){
+        fetchBoard(number: $number) {
+          number
+          writer
+          title
+          contents
+        }
+  }
+`
+
+export default function StaticRoutingMovedPage() {
+
+    const router = useRouter()
+    console.log(Number(router.query.number))
+
+    const { data } = useQuery(FETCH_BOARD, {
+        variables: {
+            number: Number(router.query.number)
+        }
+    })
+
+    const onClickMove = () => {
+        router.push(`/section09/09-04-boards/${router.query.number}/edit`)
+    }
+    console.log(data)
+
+    return(
+        <div>
+            <div>{router.query.number}번 페이지 이동이 완료되었습니다.</div>
+            <div>작성자 : {data?.fetchBoard?.writer}</div>
+            <div>제목 : {data?.fetchBoard?.title}</div>
+            <div>내용 : {data?.fetchBoard?.contents}</div>
+            <button onClick={onClickMove}>수정하러 가자~</button>
+        </div>
+    )
+}
