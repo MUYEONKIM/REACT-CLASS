a926a96731645f53920cfac500e52b41261116a8
MUYEONKIM <rlaandus2@gmail.com>
2023-06-05 04:19:36 +0900

    eslint, prettier, husky사용해서 제한하기
    eslint, prettier, husky사용해서 제한하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   const onClickMove = () => {
-    router.push('/section05/05-01-static-routing')
-  }
-  return (
-    <button onClick={onClickMove}>페이지 이동하기!!!</button>
-  )
+    router.push("/section05/05-01-static-routing");
+  };
+  return <button onClick={onClickMove}>페이지 이동하기!!!</button>;
 }
