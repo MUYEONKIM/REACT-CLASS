0e8ec1d6f761a22d62cf42ebc61622a75cf9e7b0
MUYEONKIM <rlaandus2@gmail.com>
2023-06-07 16:21:20 +0900

    layout 설정과 특정 페이지에서 레이아웃 안보이게하기

---
+++ src/components/commons/layout/index.tsx
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router";
+import LayoutBanner from "./banner";
+import LayoutFooter from "./footer";
+import LayoutHeader from "./header";
+import LayoutNavigation from "./navigation";
+
+// 변하지 않는 값은 밖에다가 써줌
+const HIDDEN_HEADERS = [
+    "/section13/13-01-libary-icon",
+    "/section13/13-02-libary-star",
+    "/",
+    // ...
+    // ..
+]
+
+interface iPropsLayout {
+    children: JSX.Element;
+}
+
+export default function Layout(props: iPropsLayout):JSX.Element {
+
+    const router = useRouter()
+    console.log("------------")
+    console.log(router.asPath)
+    console.log("------------")
+
+    const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
+
+
+    return (
+        <>
+            {!isHiddenHeader && <LayoutHeader />}
+            <LayoutBanner />
+            <LayoutNavigation />
+            <div style = {{height: "500px", display: "flex"}}>
+                <div style= {{width: "30%", backgroundColor:'orange'}}>사이드바</div>
+                <div style={{width: "70%"}}>{props.children}</div>
+            </div>
+            <LayoutFooter />
+        </>
+    )
+}
