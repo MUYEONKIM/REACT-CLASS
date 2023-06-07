8d858818d2a0014da1ae864b432bd02598cb0f68
MUYEONKIM <rlaandus2@gmail.com>
2023-06-07 11:58:46 +0900

    antd 써보기 (수정)

--- pages/section13/13-01-libary-icon/index.tsx
+++ pages/section13/13-01-libary-icon/index.tsx
@@ -1,23 +1,21 @@
 import { UpCircleOutlined } from "@ant-design/icons";
-import styled from '@emotion/styled'
-import { MouseEvent } from "react";
-import { useState } from "react";
+import styled from "@emotion/styled";
+import { useState, MouseEvent} from 'react';
 const MyIcon = styled(UpCircleOutlined)`
-    color: red;
-    font-size: 130px;
-`
+  color: red;
+  font-size: 130px;
+`;
 
 export default function LibaryIconPage(): JSX.Element {
-
-    const [ count, setCount] = useState(0)
-    const onClickDelete = (event: MouseEvent<HTMLDivElement>): void => {
-        console.log(event.currentTarget.id)
-        setCount(count + 1)
-        console.log(count)
-    }
-    return (
-        <div id="삭제" onClick={onClickDelete}>
-            <MyIcon />;
-        </div>
-    )
+  const [count, setCount] = useState(0);
+  const onClickDelete = (event: MouseEvent<HTMLDivElement>): void => {
+    console.log(event.currentTarget.id);
+    setCount(count + 1);
+    console.log(count);
+  };
+  return (
+    <div id="삭제" onClick={onClickDelete}>
+      <MyIcon />
+    </div>
+  );
 }
