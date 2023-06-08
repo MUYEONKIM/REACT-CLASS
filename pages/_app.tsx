66084a8a6f3d5df4b9f69d4e9b7fbafcb03c17d9
MUYEONKIM <rlaandus2@gmail.com>
2023-06-08 10:13:35 +0900

    apollo분리와 컴포넌트 합성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // import '/styles/globals.css'
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
+import Layout from "../src/components/commons/layout";
+import ApolloSetting from "../src/components/commons/apollo";
 
 export default function App({ Component }: AppProps): JSX.Element {
   const allStyle = {
@@ -9,19 +10,18 @@ export default function App({ Component }: AppProps): JSX.Element {
     color: "gray",
   };
 
-  const client = new ApolloClient({
-    uri: "http://backend-example.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기
-  });
+
 
   return (
     <div>
       <div style={allStyle}>
         ============== _app.js 컴포넌트 시작 부분 입니다. ================
       </div>
-      <ApolloProvider client={client}>
+      <ApolloSetting>
+        <Layout>
         <Component />
-      </ApolloProvider>
+        </Layout>
+      </ApolloSetting>
       <div style={allStyle}>
         ============== _app.js 컴포넌트 마지막 부분 입니다. ================
       </div>
---
+++ src/components/commons/apollo/index.tsx
@@ -0,0 +1,19 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+interface IApolloprops {
+    children: JSX.Element
+}
+
+export default function ApolloSetting(props: IApolloprops): JSX.Element {
+  const client = new ApolloClient({
+    uri: "http://backend-example.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기
+  });
+
+  // prettier-ignore
+  return (
+    <ApolloProvider client={client}>
+        {props.children}
+    </ApolloProvider>
+    )
+}
