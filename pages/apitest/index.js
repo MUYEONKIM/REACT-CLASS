a59230e1b328f581d524f0c63aecb51322041761
MUYEONKIM <rlaandus2@gmail.com>
2023-06-08 10:54:15 +0900

    글로벌 스타일과 css 폰트적용

---
+++ pages/apitest/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+export default function TestAPI() {
+    const [dog, setDog] = useState("")
+
+    useEffect(()=> {
+        const fetchDog = async () => {
+            const result = await.axios.get()
+        }
+    })
