bb08bcf5779a478a07709a40b1184ade66ac0ead
MUYEONKIM <rlaandus2@gmail.com>
2023-07-06 22:35:20 +0900

    stop propagation 전파 막기

--- pages/section11/11-02-stop-propagation/checkbox.tsx
+++ pages/section11/11-02-stop-propagation/checkbox.tsx
@@ -1,10 +1,10 @@
 export default function CheckBox() {
-    const qqq2 = (event) => {
+    const qqq2 = (event: React.MouseEvent<HTMLSpanElement>) => {
         event.stopPropagation()
         alert("2번 클릭")
     }
 
-    const qqq3 = (event) => {
+    const qqq3 = (event: React.MouseEvent<HTMLInputElement>) => {
         event.stopPropagation()
         alert("3번 클릭")
     }
