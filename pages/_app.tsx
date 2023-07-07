fd1f4531329e607ce86601a903461031b2dad33f
MUYEONKIM <rlaandus2@gmail.com>
2023-07-07 13:38:34 +0900

    globalstyle과 layout추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,25 @@
 import { AppProps } from "next/app";
 import Layout from "../src/components/commons/layout";
 import ApolloSetting from "../src/components/commons/apollo";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 
 export default function App({ Component }: AppProps): JSX.Element {
-  const allStyle = {
-    // 여기서 css지정해둬도 되고 import해서 지정해줘도 됨
-    fontSize: 10,
-    color: "gray",
-  };
 
 
 
   return (
     <div>
-      <div style={allStyle}>
-        ============== _app.js 컴포넌트 시작 부분 입니다. ================
-      </div>
+      <div>============== _app.js 컴포넌트 시작 부분 입니다. ================</div>
       <ApolloSetting>
-        <Layout>
-        <Component />
-        </Layout>
+        <>
+          <Global styles={globalStyles}/>
+          <Layout>
+          <Component />
+          </Layout>
+        </>
       </ApolloSetting>
-      <div style={allStyle}>
-        ============== _app.js 컴포넌트 마지막 부분 입니다. ================
-      </div>
+      <div>============== _app.js 컴포넌트 마지막 부분 입니다. ================</div>
     </div>
   );
 }
