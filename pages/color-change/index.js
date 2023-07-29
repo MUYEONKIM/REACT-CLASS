1dfca6e526186f4765662e1fe232c18bd73a58a6
MUYEONKIM <rlaandus2@gmail.com>
2023-07-29 22:09:34 +0900

    .

--- pages/color-change/index.js
+++ pages/color-change/index.js
@@ -17,7 +17,7 @@ export default function TestPage() {
     return (
         <>
             <div style = {myStyle}>
-                테스트페이지 입니다.
+                테스트페이지 입니다....
             </div>
             <button onClick={onClcikButton}>색상 바꾸기 버튼입니다</button>
             <input onChange={onChangeInput} type='text' placeholder="값을 입력해보세요."></input>
