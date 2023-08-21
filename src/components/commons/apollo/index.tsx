c32d6a3fed503e9f136e6e8d17765e37ecf9fec2
MUYEONKIM <rlaandus2@gmail.com>
2023-08-21 14:20:42 +0900

    process.browser, window, useEffect 브라우저

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink } from "@apollo
 import {createUploadLink} from 'apollo-upload-client'
 import { accessTokenState } from "../../../commons/stores";
 import { useRecoilState } from "recoil";
+import { useEffect } from "react";
 
 // 리렌더링 되면 안되니깐 컴포넌트, 따라서 컴포넌트를 불러와도 GLOBAL_STATE는 유지가 됨
 const GLOBAL_STATE = new InMemoryCache()
@@ -11,8 +12,36 @@ interface IApolloprops {
 }
 
 export default function ApolloSetting(props: IApolloprops): JSX.Element {
-  const [ accessToken ] = useRecoilState(accessTokenState)
+  const [ accessToken, setAccessToken ] = useRecoilState(accessTokenState)
 
+  // 1. process.browser
+  // if(process.browser){
+  //   const result = localStorage.getItem('accessToken');
+  //   setAccessToken(result ?? "");
+  //   console.log("나는 지금 브라우저다!")
+  // } else {
+  //   console.log(
+  //     "지금은 yarn dev 프로그램 내부다"
+  //   );
+  // };
+
+  // 2. window
+  // if(typeof window !== "undefined"){
+  //   const result = localStorage.getItem('accessToken');
+  //   setAccessToken(result ?? "");
+  //   console.log("나는 지금 브라우저다!")
+  // } else {
+  //   console.log(
+  //     "지금은 yarn dev 프로그램 내부다"
+  //   );
+  // };
+
+  // 3. useEffect
+  useEffect(() => {
+    const result = localStorage.getItem('accessToken');
+    setAccessToken(result ?? "");
+    console.log("나는 지금 브라우저다!!")
+  }, [])
   const uploadLink = createUploadLink({
     uri: "http://backend-practice.codebootcamp.co.kr/graphql",
     headers: {
