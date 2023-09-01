d4b3bcac44814c7c3fc2c9fc79679f24a866f075
MUYEONKIM <rlaandus2@gmail.com>
2023-09-01 17:36:40 +0900

    kakaomap api사용해보기

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 export default function Home(): JSX.Element {
   const router = useRouter();
   const onClickMove = (): void => {
-    void router.push("/section24/24-06-custom-hook-move-to-page");
+    void router.push("/section25/25-01-kakao-map");
   };
   return <button onClick={onClickMove}>페이지 이동하기!!!</button>;
 }
