908930041fecb78b810b3a0a767439ba5f4e6550
MUYEONKIM <rlaandus2@gmail.com>
2023-10-25 18:11:04 +0900

    마이크로 큐의 순서

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 export default function Home(): JSX.Element {
   const router = useRouter();
   const onClickMove = (): void => {
-    void router.push("/section27/27-02-web-editor-hook-form");
+    void router.push("/section30/30-01-login-refreshtoken");
   };
   return <button onClick={onClickMove}>페이지 이동하기!!!</button>;
 }
