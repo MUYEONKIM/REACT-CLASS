a018d1dff6ead75820f170bb65a10b5db08e8666
MUYEONKIM <rlaandus2@gmail.com>
2023-10-26 11:39:43 +0900

    memoization을 map과 함께 사용하기

--- pages/section31/31-03-memoization-with-map/01-parent.tsx
+++ pages/section31/31-03-memoization-with-map/01-parent.tsx
@@ -14,7 +14,7 @@ export default function MemoizationParent(): JSX.Element {
                 <MemoizationChild key={index} el={el} /> // 1. memo시, key또는 el이 변경된 부분만 리렌더링 됨
             ))} */}
             {data.split(" ").map((el, index) => (
-                <MemoizationChild key={uuidv4()} el={el} /> // 1. memo시, key또는 el이 변경된 부분만 리렌더링 됨
+                <MemoizationChild key={uuidv4()} el={el} /> // 2. memo를 해도, key자체가 변경되어 props가 넘어가므로, 다 다른것으로 인식해서 리렌더링이 전부 일어남
             ))}
             <button onClick={onClickChange}>체인지</button>
         </>
