95f368a915c8089ecedaf179676d9cfb5f291a6e
MUYEONKIM <rlaandus2@gmail.com>
2023-10-26 17:52:17 +0900

    prefetch-preload (미리 다운, 다운 순서 바꾸기)
    성능 최적화

--- src/commons/libaries/asyncFunc.ts
+++ src/commons/libaries/asyncFunc.ts
@@ -1,10 +1,16 @@
-import { FormEvent } from 'react'
+import { FormEvent } from "react";
 
-export const wrapAsyncFunc = (asyncFunc: () => Promise<void>) => () => {
-  void asyncFunc()
-};
+// prettier-ignore
+// export const wrapAsyncFunc = <E>(asyncFunc: (event: E) => Promise<void>) => (event: E) => {
+//     void asyncFunc(event);
+// }; 기존
 
-export const wrapFormAsyncFunc = (asyncFunc: () => Promise<void>) => (event: FormEvent<HTMLFormElement>) => {
-  event.preventDefault()
-  void asyncFunc()
-};
+export const wrapAsyncFunc = <E>(asyncFunc: (event: E) => Promise<void>) => (event: E) => {
+        void asyncFunc(event);
+    };
+
+export const wrapFormAsyncFunc =
+    (asyncFunc: () => Promise<void>) => (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        void asyncFunc();
+    };
