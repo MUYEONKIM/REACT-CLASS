952d49a59638880a42dc259c957d23f2e7172528
MUYEONKIM <rlaandus2@gmail.com>
2023-11-01 17:05:40 +0900

    SSR 이용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,25 +4,30 @@ import Layout from "../src/components/commons/layout";
 import ApolloSetting from "../src/components/commons/apollo";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
-import { RecoilRoot } from 'recoil'
-export default function App({ Component }: AppProps): JSX.Element {
-
-  return (
-    <div>
-      <div>============== _app.js 컴포넌트 시작 부분 입니다. ================</div>
-      <RecoilRoot>
-      <ApolloSetting>
-        <>
-          <Global styles={globalStyles}/>
-          <Layout>
-          <Component />
-          </Layout>
-        </>
-      </ApolloSetting>
-      </RecoilRoot>
-      <div>============== _app.js 컴포넌트 마지막 부분 입니다. ================</div>
-    </div>
-  );
+import { RecoilRoot } from "recoil";
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <div>
+            <div>
+                ============== _app.js 컴포넌트 시작 부분 입니다.
+                ================
+            </div>
+            <RecoilRoot>
+                <ApolloSetting>
+                    <>
+                        <Global styles={globalStyles} />
+                        <Layout>
+                            <Component {...pageProps} />
+                        </Layout>
+                    </>
+                </ApolloSetting>
+            </RecoilRoot>
+            <div>
+                ============== _app.js 컴포넌트 마지막 부분 입니다.
+                ================
+            </div>
+        </div>
+    );
 }
 // 각 파일에 있는 component의 return값들이 위의 <Component {...pageProps} />에 들어가서 실행되는 것
 // 즉 _app.js는 전체에 적용되는 파일이 되는 것
---
+++ pages/section32/32-02-opengraph-provider-with-ssr/index.tsx
@@ -0,0 +1,65 @@
+// 제공자일때 => 네이버, 다음, 쿠팡
+import { gql, useQuery } from "@apollo/client";
+import { GraphQLClient } from "graphql-request";
+import Head from "next/head";
+
+const FETCH_USEDITEM = gql`
+    query fetchUseditem($useditemId: ID!) {
+        fetchUseditem(useditemId: $useditemId) {
+            _id
+            name
+            remarks
+            images
+        }
+    }
+`;
+
+export default function OpenGraphProvider(props: any): JSX.Element {
+    console.log("나의 props", props);
+
+    const { data } = useQuery(FETCH_USEDITEM, {
+        variables: {
+            useditemId: "644f6404aef9f000281ba9da",
+        },
+    });
+
+    return (
+        <>
+            <Head>
+                <meta property="og:title" content={props?.qqq.name} />
+                <meta property="og:description" content={props?.qqq.remarks} />
+                <meta property="og:image" content={props?.qqq.images} />
+            </Head>
+            <div>중고마켓에 오신 것을 환영합니다!(여기는 Body입니다.)</div>
+        </>
+    );
+}
+
+// 이름 못 바꿈 고정임, 페이지에서 실행해야 함, app.tsx 이런데서 안됨
+// getServerSideProps는 서버에서 실행되는 것, 브라우저가 아니라
+// useEffect가 브라우저에서 실행되는 것, getServerSideProps는 제일 먼저 실행이 되는 것임
+// 가장 먼저 실행되기 때문에 여기서 백엔드에 데이터 요청 로직을 넣어 줌
+// getServerSideProps의 return값은 위의 export default의 props로 들어감
+// 프론트엔드 서버 프로그램 = webpack 서버프로그램
+export const getServerSideProps = async (): Promise<any> => {
+    console.log("여기는 서버입니다.");
+
+    // 1. 여기서 api요청
+    const graphQLclient = new GraphQLClient(
+        "https://backend-practice.codebootcamp.co.kr/graphql"
+    );
+    const result = await graphQLclient.request(FETCH_USEDITEM, {
+        useditemId: "644f6404aef9f000281ba9da",
+    });
+
+    // 2. 받은 결과를 return
+    return {
+        props: {
+            qqq: {
+                name: result.fetchUseditem.name,
+                remarks: result.fetchUseditem.remarks,
+                images: result.fetchUseditem.images,
+            },
+        },
+    };
+};
