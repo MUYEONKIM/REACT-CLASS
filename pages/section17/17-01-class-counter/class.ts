a3ae3a0e3e085b1ec670eec1854d74bab94d7248
MUYEONKIM <rlaandus2@gmail.com>
2024-02-10 19:35:41 +0900

    sdf

--- pages/section17/17-01-class-counter/class.ts
+++ pages/section17/17-01-class-counter/class.ts
@@ -15,25 +15,26 @@
 // console.log(data.getMonth() + 1)
 
 class Monster {
-  power = 50
-  attack() {
-    console.log("공격합니다!")
-  }
+    power = 50;
+    attack(): void {
+        console.log("공격합니다!");
+    }
 }
 
 // 상속
 class SuperMonster extends Monster {
-  run() {
-    console.log("도망가자!!")
-  }
+    run(): void {
+        console.log("도망가자!!");
+    }
 
-  // 오버라이딩 (덮어쓰기)
-  attack() { // 상속받아와도 다시 선언해주면 위에 것이 삭제됨
-    console.log("슈퍼몬스터 필살기")
-  }
+    // 오버라이딩 (덮어쓰기)
+    attack(): void {
+        // 상속받아와도 다시 선언해주면 위에 것이 삭제됨
+        console.log("슈퍼몬스터 필살기");
+    }
 }
-const monster = new Monster()
-monster.attach() // 공격합니다!
+const monster = new Monster();
+monster.attach(); // 공격합니다!
 
-const mymonster = new SuperMonster()
-mymonster.attack() // 슈퍼몬스터 필살기
+const mymonster = new SuperMonster();
+mymonster.attack(); // 슈퍼몬스터 필살기
