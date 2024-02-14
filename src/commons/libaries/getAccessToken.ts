292f15b9e4dcbbe735bfed3c87c49747f7db9ce0
MUYEONKIM <rlaandus2@gmail.com>
2024-02-14 23:43:58 +0900

    123

--- src/commons/libaries/getAccessToken.ts
+++ src/commons/libaries/getAccessToken.ts
@@ -11,6 +11,7 @@ const RESTORE_ACCESS_TOKEN = gql`
 
 export const getAccessToken = async (): Promise<string | undefined> => {
     try {
+        console.log("getAccessToken이 실행됬습니다");
         const graphqlClient = new GraphQLClient(
             "https://backend-practice.codebootcamp.co.kr/graphql",
             { credentials: "include" }
@@ -18,6 +19,7 @@ export const getAccessToken = async (): Promise<string | undefined> => {
         const result = await graphqlClient.request<
             Pick<IMutation, "restoreAccessToken">
         >(RESTORE_ACCESS_TOKEN);
+        console.log(result, "getaccesstoken 결과입니다");
         const newAccessToken = result.restoreAccessToken.accessToken;
         return newAccessToken;
     } catch (error) {
